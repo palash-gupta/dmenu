b35f58be5eb551e9ca97f108f871b246acaee202
Palash <palash21gupta@gmail.com>
2023-12-23 20:48:59 +0530

    emoji fonts

--- config.h
+++ config.h
@@ -2,18 +2,20 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static unsigned int border_width = 3;      /* size of the border  */
+static unsigned int border_width = 0;      /* size of the border  */
 static int centered = 0;                    /* -c option; centers dmenu on screen */
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"CaskaydiaCoveNerdFont:size=14"
+	"CaskaydiaCoveNerdFont:size=14",
+    "AppleColorEmoji:size=14",
+    "NotoColorEmoji:size=14"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#C3B1E1", "#494D64" },
-	[SchemeSel] = { "#494D64", "#c065fc" },
+	[SchemeSel] = { "#494D64", "#C3B1E1" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
