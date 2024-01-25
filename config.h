87ae42f96232554e583befb02f144d7ee16bc495
palash <palash21gupta@gmail.com>
2024-01-25 10:25:11 +0530

    alpha

--- config.h
+++ config.h
@@ -2,6 +2,7 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static const unsigned int alpha = 0xaf;     /* Amount of opacity. 0xff is opaque             */
 static unsigned int border_width = 0;      /* size of the border  */
 static int centered = 0;                    /* -c option; centers dmenu on screen */
 static int min_width = 500;                    /* minimum width when centered */
@@ -17,6 +18,12 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeNorm] = { "#C3B1E1", "#494D64" },
 	[SchemeSel] = { "#494D64", "#C3B1E1" },
 	[SchemeOut] = { "#000000", "#00ffff" },
+};
+
+static const unsigned int alphas[SchemeLast][2] = {
+	[SchemeNorm] = { OPAQUE, alpha },
+	[SchemeSel] = { OPAQUE, alpha },
+	[SchemeOut] = { OPAQUE, alpha },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 15;
